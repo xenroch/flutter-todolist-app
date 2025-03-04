6f9baf15df3c3d54f36fb8cf68e553c745f56fb0
xenroch <xenroch@gmail.com>
2025-03-05 04:29:58 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\MyBook Hype AMD\\OneDrive\\Dokumen\\devprojects\\todo_list_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\MyBook Hype AMD\\OneDrive\\Dokumen\\devprojects\\todo_list_app"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\MyBook Hype AMD\\OneDrive\\Dokumen\\devprojects\\todo_list_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\MyBook Hype AMD\\OneDrive\\Dokumen\\devprojects\\todo_list_app"
+  "FLUTTER_TARGET=C:\\Users\\MyBook Hype AMD\\OneDrive\\Dokumen\\devprojects\\todo_list_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\MyBook Hype AMD\\OneDrive\\Dokumen\\devprojects\\todo_list_app\\.dart_tool\\package_config.json"
+)
